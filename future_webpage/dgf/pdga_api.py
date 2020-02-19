ae6f8c3e22e5169db6620b54beb811dfdfb4cd6f
Federico <fes@foryouandyourcustomers.com>
2020-02-19 19:05:56 +0100

    adding urlencoder

--- future_webpage/dgf/pdga_api.py
+++ future_webpage/dgf/pdga_api.py
@@ -1,4 +1,5 @@
 import requests
+from urllib.parse import urlencode
 
 base_url = 'https://api.pdga.com/services/json'
 
@@ -24,36 +25,18 @@ def get_credentials(username, password):
 
 def query_player(credentials, first_name, last_name, pdga_number, player_class, city, state_prov, country,
                  last_modified, offset=0, limit=10):
-    query = '?'
-    if first_name:
-        query += 'first_name={}&'.format(first_name)
-
-    if last_name:
-        query += 'last_name={}&'.format(last_name)
-
-    if pdga_number:
-        query += 'pdga_number={}&'.format(pdga_number)
-
-    if player_class:
-        query += 'class={}&'.format(player_class)
-
-    if city:
-        query += 'city={}&'.format(city)
-
-    if state_prov:
-        query += 'state_prov={}&'.format(state_prov)
-
-    if country:
-        query += 'country={}&'.format(country)
-
-    if last_modified:
-        query += 'last_modified={}&'.format(last_modified)
-
-    if offset:
-        query += 'offset={}&'.format(offset)
-
-    if limit:
-        query += 'limit={}&'.format(limit)
+    query = '?' + urlencode({
+        'first_name': first_name,
+        'last_name': last_name,
+        'pdga_number': pdga_number,
+        'class': player_class,
+        'city': city,
+        'state_prov': state_prov,
+        'country': country,
+        'last_modified': last_modified,
+        'offset': offset,
+        'limit': limit
+    })
 
     requests.get('{}/players{}'.format(base_url, query),
                  headers={'Cookie': '{}={}'.format(credentials['session_name'], credentials['sessid'])})
@@ -61,39 +44,19 @@ def query_player(credentials, first_name, last_name, pdga_number, player_class, 
 
 def query_player_statistics(credentials, year, player_class, gender, division_name, division_code, country, state_prov,
                             pdga_number, last_modified, offset=0, limit=10):
-    query = '?'
-    if year:
-        query += 'year={}&'.format(year)
-
-    if player_class:
-        query += 'class={}&'.format(player_class)
-
-    if gender:
-        query += 'gender={}&'.format(gender)
-
-    if division_name:
-        query += 'division_name={}&'.format(division_name)
-
-    if division_code:
-        query += 'division_code={}&'.format(division_code)
-
-    if state_prov:
-        query += 'state_prov={}&'.format(state_prov)
-
-    if country:
-        query += 'country={}&'.format(country)
-
-    if pdga_number:
-        query += 'pdga_number={}&'.format(pdga_number)
-
-    if last_modified:
-        query += 'last_modified={}&'.format(last_modified)
-
-    if offset:
-        query += 'offset={}&'.format(offset)
-
-    if limit:
-        query += 'limit={}&'.format(limit)
+    query = '?' + urlencode({
+        'year': year,
+        'class': player_class,
+        'gender': gender,
+        'division_name': division_name,
+        'division_code': division_code,
+        'country': country,
+        'state_prov': state_prov,
+        'pdga_number': pdga_number,
+        'last_modified': last_modified,
+        'offset': offset,
+        'limit': limit
+    })
 
     requests.get('{}/player-statistics{}'.format(base_url, query),
                  headers={'Cookie': '{}={}'.format(credentials['session_name'], credentials['sessid'])})
@@ -101,39 +64,19 @@ def query_player_statistics(credentials, year, player_class, gender, division_na
 
 def query_event(credentials, tournament_id, event_name, start_date, end_date, country, state, province,
                 tier, classification, offset=0, limit=10):
-    query = '?'
-    if tournament_id:
-        query += 'tournament_id={}&'.format(tournament_id)
-
-    if event_name:
-        query += 'event_name={}&'.format(event_name)
-
-    if start_date:
-        query += 'start_date={}&'.format(start_date)
-
-    if end_date:
-        query += 'end_date={}&'.format(end_date)
-
-    if country:
-        query += 'country={}&'.format(country)
-
-    if state:
-        query += 'state={}&'.format(state)
-
-    if province:
-        query += 'province={}&'.format(province)
-
-    if tier:
-        query += 'tier={}&'.format(tier)
-
-    if classification:
-        query += 'classification={}&'.format(classification)
-
-    if offset:
-        query += 'offset={}&'.format(offset)
-
-    if limit:
-        query += 'limit={}&'.format(limit)
+    query = '?' + urlencode({
+        'tournament_id': tournament_id,
+        'event_name': event_name,
+        'start_date': start_date,
+        'end_date': end_date,
+        'country': country,
+        'state': state,
+        'province': province,
+        'tier': tier,
+        'classification': classification,
+        'offset': offset,
+        'limit': limit
+    })
 
     requests.get('{}/event{}'.format(base_url, query),
                  headers={'Cookie': '{}={}'.format(credentials['session_name'], credentials['sessid'])})
