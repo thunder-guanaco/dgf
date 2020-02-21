e7ec63fcb58aaab0ed667a4bb786530af41f10ba
Manuel García García <marianocucufate@gmail.com>
2020-02-21 01:25:51 +0100

    !25 plugin for adding friends in the banner + adding one friend in a CMS page

--- future_webpage/dgf/cms_plugins.py
+++ future_webpage/dgf/cms_plugins.py
@@ -1,6 +1,6 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
-from .models import FriendPluginModel
+from .models import FriendPluginModel, Friend
 from django.utils.translation import ugettext as _
 
 
@@ -13,4 +13,17 @@ class FriendPluginPublisher(CMSPluginBase):
 
     def render(self, context, instance, placeholder):
         context.update({'instance': instance})
+        return context
+
+
+@plugin_pool.register_plugin
+class FriendsHeaderPluginPublisher(CMSPluginBase):
+    model = FriendPluginModel
+    module = _("Disc Golf Friends")
+    name = _("Friends Header")
+    render_template = "dgf/friends_header.html"
+
+    def render(self, context, instance, placeholder):
+        context.update({'friends': Friend.objects.all()})
+        print(context)
         return context
