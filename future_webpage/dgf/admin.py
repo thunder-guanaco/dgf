09b440634e30d47e775342324ffb38cf3484c9fa
Manuel García García <marianocucufate@gmail.com>
2020-03-01 00:26:31 +0100

    21 nice friends urls with slug + nickname

--- future_webpage/dgf/admin.py
+++ future_webpage/dgf/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
-from django.utils.html import format_html
-
 
 from .models import Friend
+
 
 class FriendAdmin(admin.ModelAdmin):
 
     fieldsets = [
-            ('', {'fields': [('first_name', 'last_name'), 'pdga_number', 'city', 'main_photo']}),
+            ('', {'fields': ['username', ('first_name', 'last_name', 'nickname'), 'pdga_number', 'city', 'main_photo']}),
+            ('DANGER ZONE!', {'fields': ['slug']})
     ]
     
     list_display = ('username', 'first_name', 'last_name', 'pdga_number')
-    search_fields = ('username', 'first_name', 'last_name', 'pdga_number')
+    search_fields = ('username', 'first_name', 'last_name', 'pdga_number', 'nickname', 'slug')
+
 
 admin.site.register(Friend, FriendAdmin)
--- future_webpage/dgf/models.py
+++ future_webpage/dgf/models.py
@@ -1,12 +1,41 @@
 from django.db import models
+from django.utils.text import slugify
 from cms.models import User
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class Friend(User):
 
-    pdga_number = models.IntegerField(null=True)
-    city = models.CharField(max_length=100, null=True)
-    main_photo = models.ImageField(null=True)
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['slug'], name='unique_slug'),
+        ]
+
+    pdga_number = models.IntegerField(null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    main_photo = models.ImageField(null=True, blank=True)
+    
+    nickname = models.CharField(max_length=30, null=True, blank=True)
+    slug = models.SlugField(max_length=30, null=True, blank=True)
 
     def __str__(self):
-        return '{} {} #{}'.format(self.first_name, self.last_name, self.pdga_number)
+        pdga_number = '#{}'.format(self.pdga_number) if self.pdga_number else ''
+        return '{} {}{}'.format(self.first_name, self.last_name, pdga_number)
+
+    def save(self, *args, **kwargs):
+
+        if self.slug:
+            logger.info('Setting slug for {} to {} (slug field)'.format(self.username, self.slug))
+            self.slug = self.slug.lower()
+
+        else:
+            if self.nickname:
+                logger.info('Setting slug for {} to {} (nickname field)'.format(self.username, self.nickname))
+                self.slug = slugify(self.nickname).lower()
+            else:
+                logger.info('Setting slug for {} to {} (first_name field)'.format(self.username, self.first_name))
+                self.slug = slugify(self.first_name).lower()
+
+        super(Friend, self).save(*args, **kwargs)
