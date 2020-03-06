e4b832e9f4b24fd9250b88da8867afb4c0e0e598
Manuel García García <marianocucufate@gmail.com>
2020-03-06 17:10:04 +0100

    4 deployment

--- future_webpage/run.bash
+++ future_webpage/run.bash
@@ -2,12 +2,14 @@
 
 # VIRTUALENV + DEPENDENCIES
 . env/bin/activate
-pip3 install -r requirements.txt
+pip install -I -r requirements.txt
+
 
 #DJANGO
-python3 manage.py migrate
-export DJANGO_ENV="prod"
-export DJANGO_SECRET_KEY="delete-me"
+python manage.py migrate
+export DJANGO_ENV="dev"
+export DJANGO_SECRET_KEY="it-does-not-matter-if-you-do-this-locally"
 export DJANGO_DEBUG="True"
-export DJANGO_ALLOWED_HOSTS="vps793990.ovh.net"
-python3 manage.py runserver 0.0.0.0:8080
+export DJANGO_ALLOWED_HOSTS="localhost"
+python manage.py runserver 0.0.0.0:8000
+
