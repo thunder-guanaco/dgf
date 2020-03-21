12b41eb99b962f09ac6a7adb25be581a0a045b30
Manuel García García <marianocucufate@gmail.com>
2020-03-21 13:47:20 +0100

    new run script for all useful commands

---
+++ future_webpage/run.bash
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# CHECK ARGUMENTS
+if [[ ! "$1" =~ (test|runserver|shell|makemessages) ]]
+then
+  echo -e "\n  Usage: $0 <test, runserver, shell, makemessages>\n"
+  exit 1
+fi
+
+# VIRTUALENV + DEPENDENCIES
+. env/bin/activate
+pip install -r requirements.txt
+
+# SECRETS
+source secrets
+if [[ "$?" -eq "1" ]]
+then
+  echo
+  echo "You should get the secrets file from the server in order to have the needed environment variables"
+  echo "WARNING! This file should not be pushed to the git repository"
+  exit 1
+fi
+
+# DATABASE  + ENV
+case "$1" in
+
+  test)
+    export DJANGO_ENV="test"
+    # sqlite, no migration needed
+    ;;
+  runserver|shell|makemessages)
+    export DJANGO_ENV="dev"
+    python manage.py migrate
+    ;;
+esac
+
+# COMMAND
+case "$1" in
+
+  test)
+    python manage.py test
+    ;;
+  runserver)
+    python manage.py runserver 0.0.0.0:8000
+    ;;
+  shell)
+    python manage.py shell
+    ;;
+  makemessages)
+    python manage.py makemessages -l de -i env
+    ;;
+esac
--- future_webpage/run_makemessages_locally.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# VIRTUALENV + DEPENDENCIES
-. env/bin/activate
-pip install -r requirements.txt
-
-#DJANGO
-export DJANGO_ENV="dev"
-python manage.py makemessages -l de -i env
