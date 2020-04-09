486ede2010e97b13750ba6f22185bf6389fb3895
Federico <fes@foryouandyourcustomers.com>
2020-04-09 19:17:35 +0200

    adding all molds to the DB

--- future_webpage/dgf/apps.py
+++ future_webpage/dgf/apps.py
@@ -1,22 +1,26 @@
 import csv
+import logging
 import os
 
 from django.apps import AppConfig
 
 from .models import Disc
 
+logger = logging.getLogger(__name__)
+
 
 def load_discs():
     script_dir = os.path.dirname(__file__)
-    csv_data = open(os.path.join(script_dir, './resources/pdga-approved-disc-golf-discs.csv'))
-    csv_reader = csv.reader(csv_data, delimiter=',')
+    join = os.path.join(script_dir, './resources/pdga-approved-disc-golf-discs.csv')
+    csv_reader = csv.reader(open(join), delimiter=',')
     discs = dict()
-    row_count = 0
+    row_count = 0  # for skipping the header
     for row in csv_reader:
         if row_count != 0:
             # row[0] = manufacturer, row[1] = mold
             discs[row[1]] = row[0]
-            row_count += 1
+        row_count += 1
+    logger.info('Loaded {} discs.'.format(row_count))
     return discs
 
 
@@ -35,12 +39,13 @@ def update_approved_discs():
             new_disc.mold = mold
             new_disc.manufacturer = loaded_discs[mold]
             new_disc.save()
+            logger.info('New mold was found: {}'.format(new_disc))
 
 
 class DgfConfig(AppConfig):
+    """
+    This class loads once at the beginning (on startup).
+    That's why it is good to have the update of the approved discs done here.
+    """
     name = 'dgf'
     update_approved_discs()
-
-
-variable = load_discs()
-a = 0
--- future_webpage/dgf/test/disc_test.py
+++ future_webpage/dgf/test/disc_test.py
@@ -6,8 +6,24 @@ from ..models import Disc
 
 class DiscModelsTest(TestCase):
 
-    def test_discs_are_loaded(self):
+    # assumption: if first, last, and one in the middle discs are properly loaded, then the rest too.
+    def test_pd3_is_loaded(self):
         update_approved_discs()
-        pd3 = Disc.objects.get(model='PD3')
+        pd3 = Disc.objects.get(mold='PD3')
         self.assertEqual(pd3.mold, 'PD3')
         self.assertEqual(pd3.manufacturer, 'Discmania')
+
+    def test_wham_o_is_loaded(self):
+        update_approved_discs()
+        pd3 = Disc.objects.get(
+            mold='Professional (1, 4, 10, 14, 15, 16, 17, 20, 20 A, 21, 23A, 23B, 24A, 24B & Pro Classic molds)')
+        self.assertEqual(pd3.mold,
+                         'Professional (1, 4, 10, 14, 15, 16, 17, 20, 20 A, 21, '
+                         '23A, 23B, 24A, 24B & Pro Classic molds)')
+        self.assertEqual(pd3.manufacturer, 'Wham-O / DTW')
+
+    def test_mirus_is_loaded(self):
+        update_approved_discs()
+        pd3 = Disc.objects.get(mold='Mirus')
+        self.assertEqual(pd3.mold, 'Mirus')
+        self.assertEqual(pd3.manufacturer, 'Latitude 64')
