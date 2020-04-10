2c14ebabeea9b082d4dfe0e4899ae33b78fc8751
Federico <fes@foryouandyourcustomers.com>
2020-04-10 15:14:04 +0200

    removing comment from apps.py

--- future_webpage/dgf/apps.py
+++ future_webpage/dgf/apps.py
@@ -2,8 +2,4 @@ from django.apps import AppConfig
 
 
 class DgfConfig(AppConfig):
-    """
-    This class loads once at the beginning (on startup).
-    That's why it is good to have the update of the approved discs done here.
-    """
     name = 'dgf'
