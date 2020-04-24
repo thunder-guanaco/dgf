a793c4667159215162e0b221918388f8ba5da7d2
Manuel García García <marianocucufate@gmail.com>
2020-04-24 09:56:39 +0200

    add aces to admin and toolbar

--- future_webpage/dgf/cms_toolbars.py
+++ future_webpage/dgf/cms_toolbars.py
@@ -1,6 +1,7 @@
 from cms.toolbar_base import CMSToolbar
 from cms.toolbar_pool import toolbar_pool
 from cms.utils.urlutils import admin_reverse
+from django.utils.translation import ugettext_lazy as _
 
 
 class FriendsToolbar(CMSToolbar):
@@ -8,16 +9,36 @@ class FriendsToolbar(CMSToolbar):
     def populate(self):
         menu = self.toolbar.get_or_create_menu(
             'dgf-friends',
-            'Friends',
+            _('Friends'),
         )
         menu.add_sideframe_item(
-            name='Friends List',
+            name=_('All friends'),
             url=admin_reverse('dgf_friend_changelist'),
         )
         menu.add_modal_item(
-            name='New Friend',
+            name=_('New friend'),
             url=admin_reverse('dgf_friend_add'),
         )
 
 
 toolbar_pool.register(FriendsToolbar)
+
+
+class CourseToolbar(CMSToolbar):
+
+    def populate(self):
+        menu = self.toolbar.get_or_create_menu(
+            'dgf-course',
+            _('Courses'),
+        )
+        menu.add_sideframe_item(
+            name=_('All courses'),
+            url=admin_reverse('dgf_course_changelist'),
+        )
+        menu.add_modal_item(
+            name=_('New course'),
+            url=admin_reverse('dgf_course_add'),
+        )
+
+
+toolbar_pool.register(CourseToolbar)
