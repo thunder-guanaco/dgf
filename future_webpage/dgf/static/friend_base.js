19ffd74658c338ed83154f2e5591030975ba109c
Manuel García García <marianocucufate@gmail.com>
2020-05-06 16:31:30 +0200

    do not show non-existing videos

---
+++ future_webpage/dgf/static/friend_base.js
@@ -0,0 +1,28 @@
+$(window).on("load", function() {
+    $(".youtube-video").each(function(index) {
+
+        var video = $(this);
+        var youtube_id = video.attr("youtubeid");
+
+        img = new Image();
+        img.src = "http://img.youtube.com/vi/" + youtube_id + "/mqdefault.jpg";
+        img.onload = function () {
+            // if the video does not exist(therefore thumbnail don't exist)
+            // a default thumbnail of 120 width is returned.
+            if (this.width === 120) {
+                console.log("Youtube ID " + youtube_id + " is not valid!");
+                video.hide();
+            }
+            else {
+                this.width = 192;
+                this.height = 108;
+                video.append(this)
+                video.grtyoutube({
+                    autoPlay:true,
+                    theme: "dark"
+                });
+                video.show("slow");
+            }
+        };
+    });
+});
