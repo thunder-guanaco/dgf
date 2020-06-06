aa5da97bf195a3742e3da543879afdbe5b4129cd
Manuel García García <marianocucufate@gmail.com>
2020-06-06 23:39:16 +0200

    add silk

--- future_webpage/dgf_cms/settings.py
+++ future_webpage/dgf_cms/settings.py
@@ -171,6 +171,7 @@ TEMPLATES = [
 ]
 
 MIDDLEWARE = [
+    'silk.middleware.SilkyMiddleware',
     'cms.middleware.utils.ApphookReloadMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -189,6 +190,7 @@ INSTALLED_APPS = [
     'dgf',
     'django_crontab',
     'django_countries',
+    'silk',
 
     'djangocms_admin_style',
     'django.contrib.auth',
@@ -265,6 +267,8 @@ CMS_LANGUAGES = {
     },
 }
 
+SILKY_PYTHON_PROFILER = True
+SILKY_PYTHON_PROFILER_BINARY = True
 CONN_MAX_AGE = 60
 
 LOCALE_PATHS = [
