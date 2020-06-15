7bc8f40ed8b27480fcfb3d82d3e3d716b11b2337
Manuel García García <marianocucufate@gmail.com>
2020-06-15 12:58:03 +0200

    fixed sorting of best scores in wischlingen

--- dgf/test/templatetags/best_players_tests.py
+++ dgf/test/templatetags/best_players_tests.py
@@ -16,11 +16,23 @@ class TemplatetagsBestPlayersTest(TestCase):
         all_friends = Friend.objects.all()
         self.assertListEqual(list(dgf.order_by(all_friends, '-rating')), [kevin, fede, manolo])
 
+    def test_best_players_reverse(self):
+        manolo, kevin, fede, mario = create_friends(['manolo', 'kevin', 'fede', 'mario'],
+                                                    ratings=[883, 1007, 903, 881])
+        all_friends = Friend.objects.all()
+        self.assertListEqual(list(dgf.order_by(all_friends, 'rating')), [mario, manolo, fede])
+
     def test_best_players_someone_without_rating(self):
         manolo, kevin, wolfgang = create_friends(['manolo', 'kevin', 'wolfgang'],
                                                  ratings=[883, 1007, None])
         all_friends = Friend.objects.all()
         self.assertListEqual(list(dgf.order_by(all_friends, '-rating')), [kevin, manolo, wolfgang])
+
+    def test_best_players_someone_without_rating_reverse(self):
+        manolo, kevin, wolfgang = create_friends(['manolo', 'kevin', 'wolfgang'],
+                                                 ratings=[883, 1007, None])
+        all_friends = Friend.objects.all()
+        self.assertListEqual(list(dgf.order_by(all_friends, 'rating')), [manolo, kevin, wolfgang])
 
     def test_best_players_only_not_enough(self):
         manolo, kevin = create_friends(['manolo', 'kevin'],
