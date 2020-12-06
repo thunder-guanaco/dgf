6709ef4b0f66ca2e3857d5aaad5d0a683f0faa40
Manuel García García <marianocucufate@gmail.com>
2020-12-06 12:39:53 +0100

    show mini header on mobile scroll

--- dgf_cms/static/js/base.js
+++ dgf_cms/static/js/base.js
@@ -17,6 +17,11 @@ $(window).on("load", function() {
                 $("#mobile-navigation-bar").addClass("sticky");
                 if (!sticky) {
                     $("#mobile-navigation-bar-language-chooser").slideDown("slow");
+
+                    $("#headline img").hide("slow");
+                    $("#headline img").clone().appendTo("#mobile-mini-headline");
+                    $("#mobile-mini-headline img").slideDown("slow");
+
                     sticky = true;
                 }
             }
@@ -24,6 +29,11 @@ $(window).on("load", function() {
                 $("#mobile-navigation-bar").removeClass("sticky");
                 if (sticky) {
                     $("#mobile-navigation-bar-language-chooser").hide("slow");
+
+                    $("#headline img").show();
+                    $("#mobile-mini-headline img").slideUp("slow");
+                    $("#mobile-mini-headline img").remove();
+
                     sticky = false;
                 }
             }
