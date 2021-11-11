5474a4ab66ac21232ccc9e6d971976ad5cdc9bf1
Manuel García García <marianocucufate@gmail.com>
2021-11-11 12:18:07 +0100

    added new members

--- scripts/add_external_ids.py
+++ scripts/add_external_ids.py
@@ -5,10 +5,10 @@ friends = [
     ('alex', 126231, 1436, 'alexso', '62515'),
     ('angela', 147164, 2129, 'aceangie', '55028'),
     ('armin', 147160, 2127, 'putterbomb', '54515'),
-    ('chris', 194664, X, 'chaberno', '112752'),
+    ('chris', 194664, 2824, 'chaberno', '112752'),
     ('david', 82985, 1711, 'davido', '20851'),
     ('fede', 109371, 2106, 'fedejsoren', '39904'),
-    ('hilde', 56334, X, 'chrishilde', '25850'),
+    ('hilde', 56334, 663, 'chrishilde', '25850'),
     ('ilija', 104565, 2040, 'ilimiha', '26762'),
     ('jogi', 125551, 1847, 'jogi4711', '25547'),
     ('julia', 144087, 2237, 'julest', '54522'),
@@ -18,28 +18,34 @@ friends = [
     ('lux', 71666, 1582, 'lux', '54523'),
     ('manolo', 111828, 1922, 'manologg', '25533'),
     ('marcel', 110666, 2119, 'selloh', '44175'),
-    ('marcelr', X, X, X, '92056'),
     ('marian', 112270, 2110, 'marian', '39303'),
     ('mario', 115250, 2121, 'mariofriedrich', '48542'),
     ('markus', 94728, 1841, 'markustm', '25538'),
     ('matthias', 147730, 2488, 'thias87', '53833'),
-    ('michael', 47164, X, 'michael3001', '90199'),
     ('paul', 125227, 2327, 'paulgi', '42852'),
-    ('ralf', 91363, X, 'rollerralf', '41591'),
-    ('ralfw', 104566, X, 'rallewi', '26806'),
+    ('ralfw', 104566, 429, 'rallewi', '26806'),
     ('tobias', 127232, 2396, 'nr127232', '68205'),
     ('uli', 77646, 1769, 'ulrichberg', '23739'),
 
-    # non-PDGA members
-    ('davidb', None, X, 'gerbeerserker', '129951'),
-    ('helge', None, X, 'gmstrott', '116149'),
-    ('jesko', None, X, 'JeKo', '116360'),
-    ('lennard', None, X, 'Lennardowski', '114206'),
-    ('marcelk', None, X, 'marcelge91', '113505'),
-    ('markuss', None, X, 'cardenalmendoza', '114204'),
-    ('tana', None, X, 'tanapat', '101826'),
+    # non-PDGA
+    ('lennard', None, 3162, 'Lennardowski', '114206'),
+    ('marcelk', None, 2966, 'marcelge91', '113505'),
+    ('tana', None, 2726, 'tanapat', '101826'),
 
-    # supporting members
+    # non-GT
+    ('michael', 47164, None, 'michael3001', '90199'),
+    ('ralf', 91363, None, 'rollerralf', '41591'),
+
+    # non-PDGA non-GT
+    ('davidb', None, None, 'gerbeerserker', '129951'),
+    ('helge', None, None, 'gmstrott', '116149'),
+    ('jesko', None, None, 'JeKo', '116360'),
+    ('markuss', None, None, 'cardenalmendoza', '114204'),
+
+    # non-PDGA non-GT non-Udisc
+    ('marcelr', None, None, None, '92056'),
+
+    # supporting
     ('babara', None, None, None, None),
     ('basti', None, None, None, None),
     ('bettina', None, None, None, None),
