6a61572cab5a034a4a3756d6046237b0eda69434
Manuel García García <mgg@foryouandyourcustomers.com>
2022-03-18 12:34:21 +0100

    better UI + german translations

--- dgf/static/js/bag_tags.js
+++ dgf/static/js/bag_tags.js
@@ -26,12 +26,31 @@ multipleBagTagMode = false;
 function toggleMultipleBagTagMode() {
 
     multipleBagTagMode = !multipleBagTagMode;
+    $("#bag-tags").toggleClass("multiple-mode");
     $("#bag-tags .number").toggleClass("gray");
     $("#toggle-mode .change").toggle();
     $("#toggle-mode .cancel").toggle();
+    $("#show-bag-tags-hint").toggle();
+    $("#select-bag-tags-hint").toggle();
     $("#change-multiple-bag-tags").toggle();
 
-    if (!multipleBagTagMode) {
+    if (multipleBagTagMode) {
+
+        console.log("enter multipleBagTagMode");
+
+        $("#bag-tags .friend-ball").each(function() {
+            $(this).attr("old-href", $(this).attr("href"));
+            $(this).removeAttr("href");
+        });
+    }
+    else {
+        console.log("exiting multipleBagTagMode");
+
+        $("#bag-tags .friend-ball").each(function() {
+            $(this).attr("href", $(this).attr("old-href"));
+            $(this).removeAttr("old-href");
+        });
+
         $("#bag-tags .number").removeClass("selected");
     }
 
@@ -56,8 +75,8 @@ function bagTagClicked(bagTag) {
 
 function changeMultipleBagTags() {
 
-    $("#multiple-bag-tag-numbers li").remove();
-    $("#multiple-bag-tag-players li").remove();
+    $("#multiple-bag-tag-lists").append("<ul id='multiple-bag-tag-numbers'></ul>");
+    $("#multiple-bag-tag-lists").append("<ul id='multiple-bag-tag-players'></ul>");
 
     $("#bag-tags .content .number.selected").each(function(){
         var number = $(this).data("bag-tag");
@@ -73,8 +92,8 @@ function changeMultipleBagTags() {
         $("#bag-tags .content .player[data-bag-tag='" + number + "']").clone().appendTo(playerListItem);
     });
 
-    $("#edit-bag-tags").show();
     $("#bag-tags").hide();
+    $("#edit-bag-tags").show();
 
     $('#multiple-bag-tag-players').sortableLists({
         currElClass: 'draggedPlayer',
@@ -120,4 +139,14 @@ function multipleBagTagsSave() {
             $("#multiple-bag-tag-error").append("<span>" + response.statusText + "</span>")
         }
     });
+}
+
+function multipleBagTagsCancel() {
+
+    $("#multiple-bag-tag-numbers").remove();
+    $("#multiple-bag-tag-players").remove();
+
+    $("#edit-bag-tags").hide();
+    $("#bag-tags").show();
+
 }
