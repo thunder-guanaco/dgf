1c41c8c6f7596f7e2317c1e894bbd845f059961e
Manuel García García <marianocucufate@gmail.com>
2022-09-13 22:00:08 +0200

    reimport GT attendance admin action

--- dgf/admin.py
+++ dgf/admin.py
@@ -181,6 +181,14 @@ def recalculate_points(modeladmin, request, queryset):
 recalculate_points.short_description = _('Recalculate points')
 
 
+def reimport_attendance(modeladmin, request, queryset):
+    for tournament in queryset.filter(gt_id__isnull=False):
+        german_tour.update_tournament_attendance(tournament)
+
+
+reimport_attendance.short_description = _('Reimport attendance from turniere.discgolf.de')
+
+
 def reimport_results(modeladmin, request, queryset):
     for tournament in queryset.filter(gt_id__isnull=False):
         german_tour.update_tournament_results(tournament)
