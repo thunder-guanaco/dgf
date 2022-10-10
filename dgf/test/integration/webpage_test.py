c672a2102e21c6a7425d722d2f32feffa352347f
Manuel García García <marianocucufate@gmail.com>
2022-10-10 17:01:35 +0200

    refactored tests

--- dgf/test/integration/webpage_test.py
+++ dgf/test/integration/webpage_test.py
@@ -1,85 +1,125 @@
+from datetime import datetime, date
 from unittest import TestCase
 
 from django.test import Client, override_settings
 from django.urls import reverse
 from parameterized import parameterized
 
-from dgf.models import Friend
-from dgf.test.models.creator import create_tours, create_git_hub_issues, create_courses, create_bag_tag_changes, \
-    create_tournaments
+from dgf.models import Friend, Tournament, BagTagChange, Course, GitHubIssue, Tour
 
 LOGGED_IN = True
 NOT_LOGGED_IN = False
 
-test_friend, _ = Friend.objects.get_or_create(username='test_friend')
+
+def get_test_friend():
+    return Friend.objects.get_or_create(username='test_friend')[0]
+
+
+def get_test_tournament():
+    return Tournament.objects.get_or_create(name='Test Tournament',
+                                            defaults={
+                                                'begin': date.today(),
+                                                'end': date.today()
+                                            })[0]
+
+
+def get_test_bag_tag_change():
+    return BagTagChange.objects.get_or_create(actor=get_test_friend(),
+                                              friend=get_test_friend(),
+                                              new_number=1,
+                                              previous_number=2,
+                                              defaults={
+                                                  'timestamp': datetime.now()
+                                              })[0]
+
+
+def get_test_course():
+    return Course.objects.get_or_create(name='Test Course')[0]
+
+
+def get_test_git_hub_issue():
+    return GitHubIssue.objects.get_or_create(title='Test GitHub Issue')[0]
+
+
+def get_test_tour():
+    return Tour.objects.get_or_create(name='Test Tour')[0]
+
+
+def get_id(obj):
+    return obj.id
+
+
+def get_slug(friend):
+    return friend.slug
 
 
 class WebpageTestCase(TestCase):
 
     @parameterized.expand([
-        ('not logged in friend index', NOT_LOGGED_IN, 'dgf:friend_index', 200),
-        ('logged in friend index', LOGGED_IN, 'dgf:friend_index', 200),
+        ('friend index - not logged in', NOT_LOGGED_IN, 'dgf:friend_index', 200),
+        ('friend index - not logged in', LOGGED_IN, 'dgf:friend_index', 200),
 
-        ('not logged in friend detail', NOT_LOGGED_IN, ('dgf:friend_detail', [test_friend.slug]), 200),
-        ('logged in friend detail', LOGGED_IN, ('dgf:friend_detail', [test_friend.slug]), 200),
+        ('friend detail - not logged in', NOT_LOGGED_IN, ('dgf:friend_detail', get_test_friend, get_slug), 200),
+        ('friend detail - not logged in', LOGGED_IN, ('dgf:friend_detail', get_test_friend, get_slug), 200),
 
-        ('not logged in friend update', NOT_LOGGED_IN, 'dgf:friend_update', (302, '/login')),
-        ('logged in friend update', LOGGED_IN, 'dgf:friend_update', 200),
+        ('friend update - not logged in', NOT_LOGGED_IN, 'dgf:friend_update', (302, '/login')),
+        ('friend update - not logged in', LOGGED_IN, 'dgf:friend_update', 200),
 
-        ('not logged in feedback', NOT_LOGGED_IN, 'dgf:feedback', (302, '/login')),
-        ('logged in feedback', LOGGED_IN, 'dgf:feedback', 200),
+        ('feedback - not logged in', NOT_LOGGED_IN, 'dgf:feedback', (302, '/login')),
+        ('feedback - not logged in', LOGGED_IN, 'dgf:feedback', 200),
 
-        ('not logged in media', NOT_LOGGED_IN, 'dgf:media', 200),
-        ('logged in media', LOGGED_IN, 'dgf:media', 200),
+        ('media - not logged in', NOT_LOGGED_IN, 'dgf:media', 200),
+        ('media - not logged in', LOGGED_IN, 'dgf:media', 200),
 
-        ('not logged in tournament index', NOT_LOGGED_IN, 'dgf:tournament_index', (302, '/login')),
-        ('logged in tournament index', LOGGED_IN, 'dgf:tournament_index', 200),
+        ('tournament index - not logged in', NOT_LOGGED_IN, 'dgf:tournament_index', (302, '/login')),
+        ('tournament index - not logged in', LOGGED_IN, 'dgf:tournament_index', 200),
 
-        ('not logged in TS next tournaments', NOT_LOGGED_IN, 'dgf:tremonia_series_next_tournament',
+        ('TS next tournaments - not logged in', NOT_LOGGED_IN, 'dgf:tremonia_series_next_tournament',
          (302, 'https://discgolfmetrix.com/')),
-        ('logged in TS next tournaments', LOGGED_IN, 'dgf:tremonia_series_next_tournament',
+        ('TS next tournaments - not logged in', LOGGED_IN, 'dgf:tremonia_series_next_tournament',
          (302, 'https://discgolfmetrix.com/')),
 
-        ('not logged in TS future dates', NOT_LOGGED_IN, 'dgf:tremonia_series_future_dates', 200),
-        ('logged in future dates', LOGGED_IN, 'dgf:tremonia_series_future_dates', 200),
+        ('TS future dates - not logged in', NOT_LOGGED_IN, 'dgf:tremonia_series_future_dates', 200),
+        ('future dates - not logged in', LOGGED_IN, 'dgf:tremonia_series_future_dates', 200),
 
-        ('not logged in login', NOT_LOGGED_IN, 'login', 200),
-        ('logged in login', LOGGED_IN, 'login', 200),
+        ('login - not logged in', NOT_LOGGED_IN, 'login', 200),
+        ('login - not logged in', LOGGED_IN, 'login', 200),
 
-        ('not logged in logout', NOT_LOGGED_IN, 'logout', (302, '/')),
-        ('logged in logout', LOGGED_IN, 'logout', (302, '/')),
+        ('logout - not logged in', NOT_LOGGED_IN, 'logout', (302, '/')),
+        ('logout - not logged in', LOGGED_IN, 'logout', (302, '/')),
 
-        ('not logged in cookie consent index', NOT_LOGGED_IN, 'cookie_consent_cookie_group_list', 200),
-        ('logged in cookie consent index', LOGGED_IN, 'cookie_consent_cookie_group_list', 200),
+        ('cookie consent index - not logged in', NOT_LOGGED_IN, 'cookie_consent_cookie_group_list', 200),
+        ('cookie consent index - not logged in', LOGGED_IN, 'cookie_consent_cookie_group_list', 200),
 
-        ('not logged in friend admin', NOT_LOGGED_IN, 'admin:dgf_friend_changelist', (302, '/admin/login')),
+        ('friend admin - not logged in', NOT_LOGGED_IN, 'admin:dgf_friend_changelist', (302, '/admin/login')),
 
-        ('logged in friend admin list', LOGGED_IN, 'admin:dgf_friend_changelist', 200),
-        ('logged in friend admin add', LOGGED_IN, 'admin:dgf_friend_add', 200),
-        ('logged in friend admin change', LOGGED_IN, ('admin:dgf_friend_change', [test_friend.id]), 200),
+        ('friend admin list - not logged in', LOGGED_IN, 'admin:dgf_friend_changelist', 200),
+        ('friend admin add - not logged in', LOGGED_IN, 'admin:dgf_friend_add', 200),
+        ('friend admin change - not logged in', LOGGED_IN, ('admin:dgf_friend_change', get_test_friend, get_id), 200),
 
-        ('logged in tournament admin list', LOGGED_IN, 'admin:dgf_tournament_changelist', 200),
-        ('logged in tournament admin add', LOGGED_IN, 'admin:dgf_tournament_add', 200),
-        ('logged in tournament admin change', LOGGED_IN, ('admin:dgf_tournament_change', [create_tournaments(1).id]),
+        ('tournament admin list - not logged in', LOGGED_IN, 'admin:dgf_tournament_changelist', 200),
+        ('tournament admin add - not logged in', LOGGED_IN, 'admin:dgf_tournament_add', 200),
+        ('tournament admin change - not logged in', LOGGED_IN,
+         ('admin:dgf_tournament_change', get_test_tournament, get_id),
          200),
 
-        ('logged in bag tag change admin list', LOGGED_IN, 'admin:dgf_bagtagchange_changelist', 200),
-        ('logged in bag tag change admin add', LOGGED_IN, 'admin:dgf_bagtagchange_add', 200),
-        ('logged in bag tag change admin change', LOGGED_IN,
-         ('admin:dgf_bagtagchange_change', [create_bag_tag_changes(1).id]), 200),
+        ('bag tag change admin list - not logged in', LOGGED_IN, 'admin:dgf_bagtagchange_changelist', 200),
+        ('bag tag change admin add - not logged in', LOGGED_IN, 'admin:dgf_bagtagchange_add', 200),
+        ('bag tag change admin change - not logged in', LOGGED_IN,
+         ('admin:dgf_bagtagchange_change', get_test_bag_tag_change, get_id), 200),
 
-        ('logged in course admin list', LOGGED_IN, 'admin:dgf_course_changelist', 200),
-        ('logged in course admin add', LOGGED_IN, 'admin:dgf_course_add', 200),
-        ('logged in course admin change', LOGGED_IN, ('admin:dgf_course_change', [create_courses(1).id]), 200),
+        ('course admin list - not logged in', LOGGED_IN, 'admin:dgf_course_changelist', 200),
+        ('course admin add - not logged in', LOGGED_IN, 'admin:dgf_course_add', 200),
+        ('course admin change - not logged in', LOGGED_IN, ('admin:dgf_course_change', get_test_course, get_id), 200),
 
-        ('logged in github issue admin list', LOGGED_IN, 'admin:dgf_githubissue_changelist', 200),
-        ('logged in github issue admin add', LOGGED_IN, 'admin:dgf_githubissue_add', 200),
-        ('logged in github issue admin change', LOGGED_IN,
-         ('admin:dgf_githubissue_change', [create_git_hub_issues(1).id]), 200),
+        ('github issue admin list - not logged in', LOGGED_IN, 'admin:dgf_githubissue_changelist', 200),
+        ('github issue admin add - not logged in', LOGGED_IN, 'admin:dgf_githubissue_add', 200),
+        ('github issue admin change - not logged in', LOGGED_IN,
+         ('admin:dgf_githubissue_change', get_test_git_hub_issue, get_id), 200),
 
-        ('logged in tour admin list', LOGGED_IN, 'admin:dgf_tour_changelist', 200),
-        ('logged in tour admin add', LOGGED_IN, 'admin:dgf_tour_add', 200),
-        ('logged in tour admin change', LOGGED_IN, ('admin:dgf_tour_change', [create_tours(1).id]), 200),
+        ('tour admin list - not logged in', LOGGED_IN, 'admin:dgf_tour_changelist', 200),
+        ('tour admin add - not logged in', LOGGED_IN, 'admin:dgf_tour_add', 200),
+        ('tour admin change - not logged in', LOGGED_IN, ('admin:dgf_tour_change', get_test_tour, get_id), 200),
     ])
     @override_settings(ROOT_URLCONF='dgf.test.urls')
     def test_dgf_pages(self, name, logged_in, url, expected):
@@ -89,7 +129,8 @@ class WebpageTestCase(TestCase):
             self.login(client)
 
         if type(url) == tuple:
-            url_name, args = url
+            url_name, get_obj, get_field = url
+            args = [get_field(get_obj())]
         else:
             url_name = url
             args = []
