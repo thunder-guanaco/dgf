005a590c1298e9394e479054d016cbdb7acc0af3
Manuel García García <marianocucufate@gmail.com>
2022-10-13 23:09:02 +0200

    discgolfmetrix: JS+CSS

--- dgf_cms/static/js/discgolfmetrix.js
+++ dgf_cms/static/js/discgolfmetrix.js
@@ -11,11 +11,12 @@ $(".main-header-meta svg").after("<i class='fi-map'></i> ");
 $(".main-header-meta svg").remove();
 
 // top part: banner, breadcrumb, title
-var tsNumber = $("#competition-submenu .selected b").text().split(' ').pop();
-tsNumber = tsNumber[0] === "#" ? tsNumber : "";
+const tsNumber = $("#competition-submenu .selected b").text().split(' ').pop();
+const bannerText = tsNumber[0] === "#" ? tsNumber : "";
+const bannerDiv = "<div class='discgolffriends-banner'><img src='" + image + "'></img><span>" + bannerText + "</span></div>";
 if (typeof image !== "undefined") {
-    $(".breadcrumbs").after("<div class='discgolffriends-banner desktop'><img src='" + image + "'></img><span>" + tsNumber + "</span></div>");
-    $("#content").before("<img class='discgolffriends-banner mobile' src='" + image + "'></img>");
+    $(".breadcrumbs").after("<div class='desktop'>" + bannerDiv + "</div>");
+    $("#content").before("<div class='mobile'>" + bannerDiv + "</div>");
 }
 $(".breadcrumbs").hide();
 //$(".main-header .main-title h1").hide();
