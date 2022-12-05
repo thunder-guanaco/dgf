70dc416fb8ba5f3f4abef227e5dcbea8fac00dd5
Manuel García García <marianocucufate@gmail.com>
2022-12-05 23:26:50 +0100

    ratings update image generator

--- dgf_images/static/js/generators/ratings-update.js
+++ dgf_images/static/js/generators/ratings-update.js
@@ -2,6 +2,8 @@ $(window).on("load", function() {
 
     $("#picture-width").change(function() {
         $("#all-friends").css("width", $(this).val());
+        $("#generated-content").css("width", $(this).val());
+        $("#generated-content").css("height", $(this).val());
     }).change();
 
     $("#show-first-logo").change(function() {
@@ -12,11 +14,27 @@ $(window).on("load", function() {
         showIfChecked("#last-logo", this);
     }).change();
 
+    $("#show-banner").change(function() {
+        showIfChecked("#dgf-banner", this);
+    }).change();
+
+    $("#move-banner").change(function() {
+        $("#dgf-banner").css("margin-left", `${$(this).val()}px`);
+    }).change();
+
+
     $("#text-color").change(function() {
-        $('#generated-text').css('color', $(this).val());
+        $('#all-friends .text').css('color', $(this).val());
     }).change();
 
     $("#overlay-color").change(function() {
-        $('#overlay').css('background-color', $(this).val());
+        var backgroundColor = $(this).val();
+        var transparent = "rgba(255, 255, 255, 0)";
+        $(".mini-overlay").css("background-image", `linear-gradient(270deg, ${backgroundColor} 33%, ${transparent} 66%)`);
+    }).change();
+
+    $(".move-picture input").change(function() {
+        username = $(this).parent().parent().data("friend-username");
+        $(`#all-friends img[data-friend-username="${username}"]`).css("left", `${$(this).val()}px`);
     }).change();
 });
--- dgf_images/templatetags/dgf_images.py
+++ dgf_images/templatetags/dgf_images.py
@@ -1,8 +1,10 @@
 import logging
 
 from django import template
+from django.core.cache import cache
 
 from dgf.models import Friend
+from dgf.pdga.common import get_player_page
 
 register = template.Library()
 
@@ -18,14 +20,49 @@ def all_friends():
         .order_by('?')
 
 
+def get_rating_difference(friends):
+    for friend in friends:
+        rating_check = cache.get(f'rating_check_{friend.username}')
+        # rating_check is either:
+        #    None: there are no records of that friend
+        #    False: Friend was already checked and there's no rating difference
+        #    True: Friend was already checked and there are rating and raring difference
+
+        if rating_check is not None:
+            if not rating_check:
+                continue
+            friend.rating_difference = cache.get(f'rating_difference_{friend.username}')
+            friend.rating = cache.get(f'rating_{friend.username}')
+            yield friend
+            continue
+
+        player_page_soup = get_player_page(friend.pdga_number)
+        current_rating = player_page_soup.find('li', {'class': 'current-rating'})
+        if current_rating:
+            rating_difference = current_rating.find("a", {"class": "rating-difference"})
+            if rating_difference:
+                friend.rating_difference = rating_difference.text
+                friend.rating = [child.text.strip() for child in current_rating.children
+                                 if not child.name and child.text.strip()][0]
+                cache.set(f'rating_difference_{friend.username}', friend.rating_difference, 60 * 60 * 24)
+                cache.set(f'rating_{friend.username}', friend.rating, 60 * 60 * 24)
+                cache.set(f'rating_check_{friend.username}', True, 60 * 60 * 24)
+                yield friend
+                continue
+        cache.set(f'rating_check_{friend.username}', False, 60 * 60 * 24)
+
+
 @register.simple_tag
 def all_pdga_friends():
-    return Friend.objects \
+    friends = Friend.objects \
         .filter(social_media_agreement=True) \
         .exclude(pdga_number__isnull=True) \
+        .exclude(rating__isnull=True) \
         .exclude(main_photo__isnull=True) \
-        .exclude(main_photo='') \
-        .order_by('?')
+        .exclude(main_photo='')
+
+    friends = get_rating_difference(friends)
+    return sorted(friends, key=lambda friend: -int(friend.rating_difference))
 
 
 @register.filter
