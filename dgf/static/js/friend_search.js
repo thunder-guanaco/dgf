3c8ffba8644f61b1619f716e9af8b72d81cd45d6
Manuel García García <manuel.garciagarcia@tonies.com>
2024-01-19 09:39:58 +0100

    better UI for mobile?

--- dgf/static/js/friend_search.js
+++ dgf/static/js/friend_search.js
@@ -1,15 +1,41 @@
-$(window).on("load", function() {
-    $("#search-friend-input").on("keyup", function() {
-        searchFriends($(this).val())
-    });
+$(window).on("load", () => {
+
+//    hideAllIfMobile();
+    $("#search-friend-input").keyup(searchFriends);
+    $("#friend-search").submit(submitForm);
+
 });
 
-function searchFriends(name) {
+//function hideAllIfMobile () {
+//
+//    if (REQUEST_USER_AGENT_IS_MOBILE) {
+//        $(".all-friends .friend-wrapper").hide();
+//    }
+//}
+
+function searchFriends() {
+
+    $(".all-friends").removeClass("hidden");
+
+    var name = $(this).val();
     if (name.length > 0) {
         $(".all-friends .friend-wrapper").hide();
         $(`.all-friends .friend-wrapper[data-search*="${name.toLowerCase()}"]`).show();
     }
     else {
         $(".all-friends .friend-wrapper").show();
+    }
+}
+
+function submitForm(e) {
+
+    e.preventDefault();
+    var visibleFriends = $(".all-friends .friend-wrapper:visible()");
+    var amount = visibleFriends.length;
+    if (amount === 1) {
+        window.location = visibleFriends.first().children(".friend-ball").first().attr("href");
+    }
+    else {
+        console.log("More than one friend found! Do nothing.");
     }
 }
