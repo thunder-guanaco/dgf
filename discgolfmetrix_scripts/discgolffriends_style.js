8e0f00244b5b93345679332c99cc1e908502177a
Manuel García García <manuel.garciagarcia@tonies.com>
2024-02-26 22:12:55 +0100

    simplify DGF js code + use generic DGM functions

--- discgolfmetrix_scripts/discgolffriends_style.js
+++ discgolfmetrix_scripts/discgolffriends_style.js
@@ -2,9 +2,6 @@
     // only needed if you only want to show future subcompetitions (not all) like in Tremonia Series
     showOnlyFutureSubcompetitions = false;
 
-    // only needed if you want to have a banner
-    image = "https://discgolffriends.de/static/img/banners/tremonia-series.png";
-
     $.ajax({
         url: "https://discgolffriends.de/static/js/discgolfmetrix.js",
         success: function(response) {
