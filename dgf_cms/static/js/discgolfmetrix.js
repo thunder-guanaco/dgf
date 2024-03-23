74b8d95318ce0c3bb0d0643d91a81b503716becb
Manuel García García <manuel.garciagarcia@tonies.com>
2024-03-23 23:27:34 +0100

    updated discgolfmetrix script/style

--- dgf_cms/static/js/discgolfmetrix.js
+++ dgf_cms/static/js/discgolfmetrix.js
@@ -17,28 +17,42 @@ function loadDiscGolfMetrixScript() {
 
 }
 
-var DGF_LOGO_IMG = "<img class='dgf-logo' src='https://discgolffriends.de/static/img/logo.png' width='40px'/>"
+var DGF_LOGO_IMG = "<img class='dgf-logo' src='https://discgolffriends.de/static/img/logo.png' width='40px'/>";
+var DGF_BAG_TAG = (number) => `<div class='bag-tag-number' style='display: none;'><span>${number}</span></div>`;
+var DGF_PART = (number) => `<div class='dgf-part'>${DGF_LOGO_IMG}${DGF_BAG_TAG(number)}</div>`;
 
 function markFriends() {
     $.ajax({
         type: "GET",
-        url: "https://discgolffriends.de/friends/disc-golf-metrix/all-friend-ids",
+        url: "https://discgolffriends.de/friends/disc-golf-metrix/friends",
         success: function(response) {
 
-            response.ids.forEach(id => {
+            metrix_friends = response.friends;
+
+            var show_bag_tags = false;
+
+            response.friends.forEach(friend => {
+
+                if (user_id == friend.metrix_user_id) {
+                    show_bag_tags = true;
+                }
 
                 // results page
-                $(`a.profile-link[href="/player/${id}"]`)
+                $(`a.profile-link[href="/player/${friend.metrix_user_id}"]`)
                     .hide()
-                    .first().prepend(DGF_LOGO_IMG).show()
+                    .first().prepend(DGF_PART(friend.bag_tag)).show()
                     .children("svg").remove();
 
                 // registration page
-                $(`a:not(.profile-link)[href="/player/${id}"]`)
+                $(`a:not(.profile-link)[href="/player/${friend.metrix_user_id}"]`)
                     .parent()
-                    .append(DGF_LOGO_IMG);
+                    .append(DGF_PART(friend.bag_tag));
 
             });
+
+            if (show_bag_tags) {
+                $(".bag-tag-number").show();
+            }
         }
     });
 }
